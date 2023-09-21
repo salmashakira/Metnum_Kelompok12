5c0ec45c45db6eb1305e102cd708b86faf46fa2c
Salma Shakira Nurul Azmi <144917212+salmashakira@users.noreply.github.com>
2023-09-21 09:59:24 +0700

    Add files via upload

---
+++ 2205521_Citra Ayu Rahmawati_Kelompok12/Metnum_No1_b_kelompok12.py
@@ -0,0 +1,30 @@
+'''
+METNUM Kelompok 12
+No. 1
+b. f(x) = e^x - x
+'''
+import numpy as np
+
+def my_bisection(func, a, b, tol=1e-6, max_iterations=100):
+    iterations = 0
+    while iterations < max_iterations:
+        c = (a + b) / 2
+        if func(c) == 0 or (b - a) / 2 < tol:
+            return c
+        if np.sign(func(c)) == np.sign(func(a)):
+            a = c
+        else:
+            b = c
+        iterations += 1
+    return None
+
+# Fungsi baru: f(x) = e^x - x
+f = lambda x: np.exp(x) - x
+
+r1 = my_bisection(f, 0, 2, 0.1)
+print("r1 =", r1)
+print("f(r1) =", f(r1))
+
+r01 = my_bisection(f, 0, 2, 0.01)
+print("r01 =", r01)
+print("f(r01) =", f(r01))
